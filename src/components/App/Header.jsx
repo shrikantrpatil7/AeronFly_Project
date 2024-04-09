4854b3f64d714aa8da079f49a9c58da6a033c046
shrikantrpatil7 <118796468+shrikantrpatil7@users.noreply.github.com>
2024-04-09 15:42:18 +0530

    Update Header.jsx
    update this file with "className" to "class"

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -11,60 +11,60 @@ const Header = ({ rtl }) => {
   }
 
   return (
-    <header className="style-4" data-scroll-index="0">
-      <div className="content">
-        <div className="container">
-          <div className="row gx-0">
-            <div className="col-lg-6">
-              <div className="info">
-                {/* <small className="mb-50 title_small">{ rtl ? 'نوتيرو - تطبيق مذكرة سهل الاستخدام' : 'Notero - Easy Notes App' }</small> */}
-                <h1 className="mb-30">{ rtl ? 'احفظ' : 'Bill Payment' } <span>{ rtl ? 'كل شئ' : 'Made Easy  With' }</span> { rtl ? 'على التطبيق' : 'AeronPay' }</h1>
-                <p className="text">{ rtl ? 'لا يلزم الترميز لإجراء التخصيصات. يحتوي المُخصص المباشر' : 'One App For All Your Financial Needs , Start Your Payment' }<br />{ rtl ? 'على كل ما تحتاجه.' : ' Journey With AeronPay.' }</p>
-                <div className="d-flex align-items-center mt-50">
-                  <a href="https://www.apple.com/app-store" rel="noreferrer" className="btn rounded-pill bg-blue4 fw-bold text-white me-4" target="_blank">
-                    <small> <i className="fab fa-apple me-2 pe-2 border-end"></i>{ rtl ? 'تحميل التطبيق' : 'Download App' }</small>
+    <header class="style-4" data-scroll-index="0">
+      <div class="content">
+        <div class="container">
+          <div class="row gx-0">
+            <div class="col-lg-6">
+              <div class="info">
+                {/* <small class="mb-50 title_small">{ rtl ? 'نوتيرو - تطبيق مذكرة سهل الاستخدام' : 'Notero - Easy Notes App' }</small> */}
+                <h1 class="mb-30">{ rtl ? 'احفظ' : 'Bill Payment' } <span>{ rtl ? 'كل شئ' : 'Made Easy  With' }</span> { rtl ? 'على التطبيق' : 'AeronPay' }</h1>
+                <p class="text">{ rtl ? 'لا يلزم الترميز لإجراء التخصيصات. يحتوي المُخصص المباشر' : 'One App For All Your Financial Needs , Start Your Payment' }<br />{ rtl ? 'على كل ما تحتاجه.' : ' Journey With AeronPay.' }</p>
+                <div class="d-flex align-items-center mt-50">
+                  <a href="https://www.apple.com/app-store" rel="noreferrer" class="btn rounded-pill bg-blue4 fw-bold text-white me-4" target="_blank">
+                    <small> <i class="fab fa-apple me-2 pe-2 border-end"></i>{ rtl ? 'تحميل التطبيق' : 'Download App' }</small>
                   </a>
 
-                  <a href="https://www.apple.com/app-store" rel="noreferrer" className="btn rounded-pill bg-blue4 fw-bold text-white me-4" target="_blank">
-                    <small>  <i className="fab fa-google-play me-2 pe-2 border-end"></i> { rtl ? 'تحميل التطبيق' : 'Google Play' }</small>
+                  <a href="https://www.apple.com/app-store" rel="noreferrer" class="btn rounded-pill bg-blue4 fw-bold text-white me-4" target="_blank">
+                    <small>  <i class="fab fa-google-play me-2 pe-2 border-end"></i> { rtl ? 'تحميل التطبيق' : 'Google Play' }</small>
                   </a>
 
-                  {/* <a href="https://youtu.be/pGbIOC83-So?t=21" data-lity className="play-btn" onClick={openVideo}>
-                    <span className="icon me-2">
-                      <i className="fas fa-play ms-1"></i>
+                  {/* <a href="https://youtu.be/pGbIOC83-So?t=21" data-lity class="play-btn" onClick={openVideo}>
+                    <span class="icon me-2">
+                      <i class="fas fa-play ms-1"></i>
                     </span>
-                    <strong className="small">{ rtl ? 'مشاهدة' : 'View' } <br />{ rtl ? 'البرومو' : 'Promotion' }</strong>
+                    <strong class="small">{ rtl ? 'مشاهدة' : 'View' } <br />{ rtl ? 'البرومو' : 'Promotion' }</strong>
                   </a> */}
                 </div>
-                <span className="mt-100 me-5">
+                <span class="mt-100 me-5">
                   <small
-                    className="icon-30 bg-gray rounded-circle color-blue4 d-inline-flex align-items-center justify-content-center me-2">
-                    <i className="fas fa-sync"></i>
+                    class="icon-30 bg-gray rounded-circle color-blue4 d-inline-flex align-items-center justify-content-center me-2">
+                    <i class="fas fa-sync"></i>
                   </small>
-                  <small className="text-uppercase">{ rtl ? 'مجانى لمده 14 يوم' : 'Free 14 Days Trial' }</small>
+                  <small class="text-uppercase">{ rtl ? 'مجانى لمده 14 يوم' : 'Free 14 Days Trial' }</small>
                 </span>
-                <span className="mt-100">
+                <span class="mt-100">
                   <small
-                    className="icon-30 bg-gray rounded-circle color-blue4 d-inline-flex align-items-center justify-content-center me-2">
-                    <i className="fas fa-credit-card"></i>
+                    class="icon-30 bg-gray rounded-circle color-blue4 d-inline-flex align-items-center justify-content-center me-2">
+                    <i class="fas fa-credit-card"></i>
                   </small>
-                  <small className="text-uppercase">{ rtl ? 'دفعة لمرة واحدة' : 'One time payment' }</small>
+                  <small class="text-uppercase">{ rtl ? 'دفعة لمرة واحدة' : 'One time payment' }</small>
                 </span>
               </div>
             </div>
-            <div className="col-lg-6">
-              <div className="img">
+            <div class="col-lg-6">
+              <div class="img">
                 {/* <img src="/assets/img/header/header_4.png" alt="" />  */}
                
                 
 <div class="col">
 <div class="thumbnail">
 
-<video className="banner-video ind-vid-rem purchase-img-light" loop autoPlay muted playsInline>
+<video class="banner-video ind-vid-rem purchase-img-light" loop autoPlay muted playsInline>
  <source src="/assets/img/header/home-bann.mp4" type="video/mp4" />
 </video> 
 
-<video className="banner-video1 ind-vid-res purchase-img-light" loop autoPlay muted playsInline>
+<video class="banner-video1 ind-vid-res purchase-img-light" loop autoPlay muted playsInline>
  <source src="/assets/img/header/home-ban1.mp4" type="video/mp4" />
 </video>
 
@@ -77,9 +77,9 @@ const Header = ({ rtl }) => {
             </div>
           </div>
         </div>
-        <img src="/assets/img/header/header_4_bubble.png" alt="" className="bubble" />
+        <img src="/assets/img/header/header_4_bubble.png" alt="" class="bubble" />
       </div>
-      <img src="/assets/img/header/header_4_wave.png" alt="" className="wave" />
+      <img src="/assets/img/header/header_4_wave.png" alt="" class="wave" />
       {
         typeof window !== "undefined" && 
           (
